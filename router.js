e629d0779e032aca469dd3fe0f2ff0110294ecc5
Chuck King <cwkingjr@gmail.com>
2016-08-25 15:06:53 -0500

    additional cleanup

--- router.js
+++ router.js
@@ -10,37 +10,37 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireLogin = passport.authenticate('local', { session: false });
 
 // Constants for role types
-const REQUIRE_ADMIN = "Admin";
-const REQUIRE_OWNER = "Owner";
-const REQUIRE_MEMBER = "Member";
+const ROLE_ADMIN = "Admin";
+const ROLE_OWNER = "Owner";
+const ROLE_MEMBER = "Member";
 
 module.exports = function(app) {
-  // Initializing route groups
-  const apiRoutes = express.Router();
-  const authRoutes = express.Router();
-  const mailgunRoutes = express.Router();
-  const userRoutes = express.Router();
+	// Initializing route groups
+	const apiRoutes = express.Router();
+	const authRoutes = express.Router();
+	const mailgunRoutes = express.Router();
+	const userRoutes = express.Router();
 
-  // Set these routes as a subdir of apiRoutes
-  // e.g., /api/auth/login, /api/user/, etc
-  apiRoutes.use('/auth', authRoutes);
-  apiRoutes.use('/user', userRoutes);
-  apiRoutes.use('/communication', mailgunRoutes);
+	// Set these routes as a subdir of apiRoutes
+	// e.g., /api/auth/login, /api/user/, etc
+	apiRoutes.use('/auth', authRoutes);
+	apiRoutes.use('/user', userRoutes);
+	apiRoutes.use('/communication', mailgunRoutes);
 
-  authRoutes.post('/register', authController.register);
-  authRoutes.post('/login', requireLogin, authController.login);
-  authRoutes.post('/forgot-password', authController.forgotPassword);
-  authRoutes.post('/reset-password/:token', authController.verifyToken);
+	authRoutes.post('/register', authController.register);
+	authRoutes.post('/login', requireLogin, authController.login);
+	authRoutes.post('/forgot-password', authController.forgotPassword);
+	authRoutes.post('/reset-password/:token', authController.verifyToken);
 
-  mailgunRoutes.post('/contact', mailgunController.sendContactForm);
+	mailgunRoutes.post('/contact', mailgunController.sendContactForm);
 
-  userRoutes.get('/:userId', requireAuth, userController.viewProfile);
+	userRoutes.get('/:userId', requireAuth, userController.viewProfile);
 
-  // Temp protected route to make sure token authentication is working
-  apiRoutes.get('/protected', requireAuth, function(req, res) {
-    res.send({ content: 'The protected test route is functional!'});
-  });
+  	// Temp protected route to make sure token authentication is working
+	apiRoutes.get('/protected', requireAuth, function(req, res) {
+		res.send({ content: 'The protected test route is functional!'});
+	});
 
-  // Set url for API group routes
-  app.use('/api', apiRoutes);
+	// Set url for API group routes
+	app.use('/api', apiRoutes);
 };
--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use(logger('dev'));
 
 // Enable CORS from client-side
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
+	res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+	res.header("Access-Control-Allow-Credentials", "true");
+	next();
 });
 
 // Import routes to be served
